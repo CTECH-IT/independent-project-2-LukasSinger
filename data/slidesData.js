d84f85a166ed91c40483d79c42e2f28e7dbaec7a
Lukas Singer <7627101@isd535.org>
2022-03-03 11:30:08 -0600

    Started director slides section

---
+++ data/slidesData.js
@@ -0,0 +1,58 @@
+export const slidesData = [
+  {
+    name: "Rick Kvam",
+    subtitle: "Artistic Director & Concert Choir Conductor",
+    description:
+      "Dr. Rick Kvam holds music degrees from Harvard College and Cincinnati College-Conservatory of Music.  He founded Rochester Men’s Capella in 1982, Choral Arts Ensemble in 1985, and Honors Choirs of Southeast Minnesota in 1992. His Choral Arts Ensemble has sung with Saint Paul Chamber Orchestra and appeared on Prairie Home Companion, and sang most recently in a production of The Events at the Guthrie Theater (Minneapolis, MN). His Honors Concert Choir has appeared at the national convention of Chorus America, New York City’s Lincoln Center, and has been repeatedly selected for national Best of High School a Cappella compilations. Dr. Kvam currently serves as Artistic Director and conductor for both organizations. In 2014, he received the Leadership in the Arts award given by the Greater Rochester Arts and Cultural Trust. Dr. Kvam was inducted into the Minnesota Music Hall of Fame in 2016.",
+    url: ""
+  },
+  {
+    name: "Aaron Schumacher",
+    subtitle: "Chorale Conductor",
+    description:
+      "Aaron Schumacher is beginning his 4th year of directing Chorale.  He holds a bachelor of arts degree in vocal and instrumental music from Concordia College in Moorhead, MN, where he sang under the direction of Dr. Rene Clausen as a member of The Concordia Choir.  He is in his 13th year of teaching at Zumbrota Mazeppa Schools, having focused on vocal and classroom music at the middle school level.  During his time at ZM Schools he has also taught band lessons, directed musicals, coached Mock Trial teams, and been a Peer Coach for fellow teachers.  He is a member of Choral Arts Ensemble.",
+    url: ""
+  },
+  {
+    name: "Bill Nelson",
+    subtitle: "Treble Choir Conductor",
+    description:
+      "Bill Nelson is leading the Treble Choir for his 21st season. He holds a bachelor of arts degree in vocal and instrumental music from Luther College (IA) where he studied with Weston Noble and was a member of the Nordic Choir and the Concert Band. Previously a middle school and high school choir director, he currently is a CERTIFIED FINANCIAL  PLANNER® professional with Think Mutual Bank. He has been a member of the Choral Arts Ensemble, and directs a choir at Zion Lutheran Church in Stewartville.",
+    url: ""
+  },
+  {
+    name: "Amy Nelson",
+    subtitle: "Children's Chorus Conductor",
+    description:
+      "Amy Nelson is in her 20th season leading Children's Chorus. She holds a bachelor of arts degree in vocal music from Luther College (IA) where she sang in the Nordic Choir under the direction of Weston Noble. She has also been a member of the Choral Arts Ensemble. Previously a music teacher and private voice instructor, she currently leads Sunday school music and directs a children’s choir at Zion Lutheran Church in Stewartville. ",
+    url: ""
+  },
+  {
+    name: "Korrie Johnson",
+    subtitle: "Half Notes Conductor",
+    description:
+      "Korrie Johnson is the Half Notes instructor, and is in her 6th year of conducting this young ensemble. She hold a bachelor of science degree in vocal music education from Minnesota State University, Mankato, where she sang in the Chamber Singers and Concert Choir under the direction of Dr. David Dickau. She taught music in Rochester Public Schools, was a Music Education specialist for Honors Choirs, has taught for SEMYO, and has been a private voice teacher. She was a member of Choral Arts Ensemble.",
+    url: ""
+  },
+  {
+    name: "Jan Kvam",
+    subtitle: "Treble Choir & Concert Choir Pianist",
+    description:
+      "Jan Kvam holds a piano performance degree from Bethel College in St. Paul, MN, and keeps an active schedule as collaborative pianist for a wide variety of ensembles and soloists throughout the SE Minnesota region. Jan is enjoying her 19th season playing for Honors Choirs.",
+    url: ""
+  },
+  {
+    name: "Jon Davis",
+    subtitle: "Chorale and Half Notes Pianist",
+    description:
+      "Jon has a bachelor of arts in music (emphasis in piano performance) from Mankato State University, plays piano for a big band in Rochester called Swing Street, frequently plays with D’Sievers jazz combo in various venues, and has been the pianist and/or musical director for almost 80 musicals. Jon begins his 6th season with Honors Choirs.",
+    url: ""
+  },
+  {
+    name: "Cindy Breederland",
+    subtitle: "Children's Chorus Pianist",
+    description:
+      "Cindy Breederland holds a bachelor of music degree from Cornerstone University in Grand Rapids, Michigan and is a Nationally Certified Teacher of Music. Cindy has served as a church musician for many years, and also enjoys teaching piano and playing oboe in the Cannon Valley Regional Orchestra. She is in her 9th season with Honors Choirs.",
+    url: ""
+  }
+];
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-// Put your JavaScript here
+import { slidesData } from "../data/slidesData.js";
+
+const SLIDES_CONTAINER_SELECTOR = '[data-slides-role="container"]';
+
+const slidesContainer = document.querySelector(SLIDES_CONTAINER_SELECTOR);
+
+function populateSlidesContainer() {
+  for (slide of slidesData) {
+    const img = document.createElement("img");
+    img.classList.add("slide-image");
+    img.setAttribute("data-slides-role", "image");
+    slide.element = document.createElement("div");
+    slide.element.appendChild();
+    slidesContainer.appendChild(slide.element);
+  }
+}

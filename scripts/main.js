03cacaa942813d0d00c007d5282bd7470424f904
Lukas Singer <7627101@isd535.org>
2022-03-07 11:34:36 -0600

    Finished director section layout, started styling

--- scripts/main.js
+++ scripts/main.js
@@ -6,27 +6,62 @@ const DETAIL_IMAGE_SELECTOR = `[${DETAIL_IMAGE_ATTRIBUTE}="true"]`;
 
 const slidesContainer = document.querySelector(SLIDES_CONTAINER_SELECTOR);
 
-function onSlideClick(e) {
-  const imgContainer = e.target;
-  if (imgContainer.getAttribute(DETAIL_IMAGE_ATTRIBUTE) != "true") {
-    const oldDetail = document.querySelector(DETAIL_IMAGE_SELECTOR);
+function setAsDetail(element) {
+  // Revert the old detail image (if any) to a regular image
+  const oldDetail = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  if (oldDetail) {
     oldDetail.setAttribute(DETAIL_IMAGE_ATTRIBUTE, "false");
-    imgContainer.classList.add("detail");
-    imgContainer.setAttribute(DETAIL_IMAGE_ATTRIBUTE, "true");
+    oldDetail.classList.remove("detail");
+  }
+  // Convert the specified element to a detail image
+  element.classList.add("detail");
+  element.setAttribute(DETAIL_IMAGE_ATTRIBUTE, "true");
+}
+
+function onSlideClick() {
+  const imgContainer = this;
+  if (imgContainer.getAttribute(DETAIL_IMAGE_ATTRIBUTE) != "true") {
+    setAsDetail(imgContainer);
   }
 }
 
 function populateSlidesContainer() {
-  for (slide of slidesData) {
+  for (let slide of slidesData) {
+    // Create the image
     const img = document.createElement("img");
     img.classList.add("slide-image");
-    img.setAttribute("data-slides-role", "image");
+    img.setAttribute("src", slide.url);
+    // Create the image container and add the image
     const imgContainer = document.createElement("div");
     imgContainer.classList.add("slide-image-container");
-    imgContainer.setAttribute("data-slides-role", "image-container");
-    imgContainer.addEventListener("click", onSlideClick);
     imgContainer.appendChild(img);
-    slidesContainer.appendChild(imgContainer);
+    imgContainer.addEventListener("click", onSlideClick);
+    // Create the name, subtitle, and description text blocks
+    const name = document.createElement("span");
+    name.classList.add("slide-name");
+    name.textContent = slide.name;
+    const subtitle = document.createElement("span");
+    subtitle.classList.add("slide-subtitle");
+    subtitle.textContent = slide.subtitle;
+    const description = document.createElement("span");
+    description.classList.add("slide-description");
+    description.textContent = slide.description;
+    // Create the text container and add the text elements
+    const textContainer = document.createElement("div");
+    textContainer.classList.add("slide-text-container");
+    textContainer.appendChild(name);
+    textContainer.appendChild(subtitle);
+    textContainer.appendChild(description);
+    // Create the container for both slide elements
+    const slideContainer = document.createElement("div");
+    slideContainer.classList.add("slide-container");
+    slideContainer.appendChild(imgContainer);
+    slideContainer.appendChild(textContainer);
+    // Add all of the elements to the page
+    slidesContainer.appendChild(slideContainer);
+
+    // Set first image as detail on load
+    if (slide == slidesData[0]) setAsDetail(imgContainer);
   }
 }
 

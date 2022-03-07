5aaa46219ed7838edf31b3019f67a784a23b857e
LukasSinger <spelled.with.k@gmail.com>
2022-03-07 10:01:58 -0600

    Continued work on director slides

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,33 @@
 import { slidesData } from "../data/slidesData.js";
 
 const SLIDES_CONTAINER_SELECTOR = '[data-slides-role="container"]';
+const DETAIL_IMAGE_ATTRIBUTE = "detail-image";
+const DETAIL_IMAGE_SELECTOR = `[${DETAIL_IMAGE_ATTRIBUTE}="true"]`;
 
 const slidesContainer = document.querySelector(SLIDES_CONTAINER_SELECTOR);
+
+function onSlideClick(e) {
+  const imgContainer = e.target;
+  if (imgContainer.getAttribute(DETAIL_IMAGE_ATTRIBUTE) != "true") {
+    const oldDetail = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    oldDetail.setAttribute(DETAIL_IMAGE_ATTRIBUTE, "false");
+    imgContainer.classList.add("detail");
+    imgContainer.setAttribute(DETAIL_IMAGE_ATTRIBUTE, "true");
+  }
+}
 
 function populateSlidesContainer() {
   for (slide of slidesData) {
     const img = document.createElement("img");
     img.classList.add("slide-image");
     img.setAttribute("data-slides-role", "image");
-    slide.element = document.createElement("div");
-    slide.element.appendChild();
-    slidesContainer.appendChild(slide.element);
+    const imgContainer = document.createElement("div");
+    imgContainer.classList.add("slide-image-container");
+    imgContainer.setAttribute("data-slides-role", "image-container");
+    imgContainer.addEventListener("click", onSlideClick);
+    imgContainer.appendChild(img);
+    slidesContainer.appendChild(imgContainer);
   }
 }
+
+populateSlidesContainer();
